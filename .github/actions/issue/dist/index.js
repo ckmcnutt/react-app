01de0c2fc0d541763df3dbe5e0d128ac24237d23
Chris McNutt <chris.mcnutt@mutualofomaha.com>
2022-03-21 14:19:05 -0500

    ci: typo in actions JS

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -952,7 +952,7 @@ async function main() {
     const body = core.getInput("body");
     const assignees = core.getInput("assignees");
 
-    const octokit = new github.Github(token);
+    const octokit = new github.GitHub(token);
     const res = await octokit.issues.create({
       ...github.context.repo,
       title,
