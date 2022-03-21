a11d240c2a0821e87b27905481546c05d70be8d2
Chris McNutt <chris.mcnutt@mutualofomaha.com>
2022-03-21 13:49:21 -0500

    ci: recompile action

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -881,13 +881,23 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
+  // debug messages
+  core.debug("Test debug message");
+  core.warning("Test warning message");
+  core.error("Test error message");
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name)
   console.log(`Hello ${name}!`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object")
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello world!")
 } catch (error) {
     core.setFailed(error.message);
 }
