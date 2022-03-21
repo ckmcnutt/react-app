d91e1e712165e038b31c7810de88d35fead79479
Chris McNutt <chris.mcnutt@mutualofomaha.com>
2022-03-21 13:46:06 -0500

    ci: test actions 2

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,13 +2,23 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  // debug messages
+  core.debug("Test debug message");
+  core.warning("Test warning message");
+  core.error("Test error message");
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name)
   console.log(`Hello ${name}!`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object")
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello world!")
 } catch (error) {
     core.setFailed(error.message);
 }
